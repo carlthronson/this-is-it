60a69e69fc98805df74f7a4627f69749794fff61
Carl Thronson <carlthronson@gmail.com>
2023-08-20 13:28:26 -0700

    Add link for client side navigation to shop-ease page

--- app/page.js
+++ app/page.js
@@ -1,4 +1,13 @@
 // `app/page.js` is the UI for the `/` URL
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>Hello World, This is the home page of this app!</h1>
+  return (
+    <div>
+      <h1>Hello World, This is the home page of this app!</h1>
+      <Link href="/shop-ease">
+        <a>ShopEase</a>
+      </Link>
+    </div>
+  )
 }
